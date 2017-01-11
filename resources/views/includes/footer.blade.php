ff3e80b7d3995488d76a686cf0478068259c1e72
kenderzambrano <kenderzambrano1@gmail.com>
2017-01-11 07:00:59 -0400

    Inclusion
    kz

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,7 @@
+<div id="copyright text-right">© Copyright 2016 - ICLF</div>
+
+<footer class="footer">
+  <div class="container">
+    <p class="text-muted">© Copyright 2016 - ICLF</p>
+  </div>
+</footer>
---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,11 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="author" content="Kender Zambrano">
+
+<title>ICLF</title>
+
+<!-- load bootstrap from a cdn -->
+<!-- <link rel="stylesheet" href="//netdna.bootstrapcdn.com/twitter-bootstrap/3.0.3/css/bootstrap-combined.min.css"> -->
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
